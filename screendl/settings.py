03e519eac13e3ec845d618360f5a7fb74ac11d6c
greg <grgfa@yahoo.com>
2021-04-27 21:39:40 +0000

    Changed some settings

--- screendl/settings.py
+++ screendl/settings.py
@@ -24,9 +24,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = environ['secret_key']
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '0.0.0.0', 'localhost', '127.0.0.1', 'moviedlapi.herokuapp.com'
+]
 
 
 # Application definition
